970c91f09b059d229b9d72cbe7d6143d21e19f5d
0404233 <andrey0404233@gmail.com>
2025-01-20 23:04:56 +0300

    feat: done 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+const process = require('process');
+
+const pathToFile = path.resolve(__dirname, 'text.txt');
+
+const writeFileAsync = (path, data) => {
+  return new Promise((res, rej) => {
+    fs.writeFile(path, data, (err) => {
+      if (err) rej(err);
+      res();
+    })
+  })
+}
+
+const appendFileAsync = (path, data) => {
+  return new Promise((res, rej) => {
+    fs.appendFile(path, data, (err) => {
+      if (err) rej(err);
+      res();
+    })
+  })
+}
+
+function exit() {
+  console.log('Thank you! Goodbye!');
+  process.exit(0);
+}
+
+writeFileAsync(pathToFile, '')
+  .then(() => {
+    process.stdout.write('File is created! \n');
+    process.stdin.on('data', (data) => {
+      if (data.toString().trim() === '.exit') {
+        exit();
+      }
+      appendFileAsync(pathToFile, data)
+        .then(() => console.log('Data appended!'))
+        .catch((err) => console.error('Error appending data:', err));
+    })
+  })
+  .catch((err) => console.error('Error creating file:', err));
+
+process.on('SIGINT', () => {
+  console.log('Thank you! Goodbye!');
+  process.exit(0);
+});
