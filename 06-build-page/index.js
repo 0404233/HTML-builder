e3cd98bc2fce7a9abfb1ffaa40efd5375ee641f8
0404233 <andrey0404233@gmail.com>
2025-01-21 02:35:59 +0300

    feat: implement createHTML fucnc in 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,6 +3,9 @@ const fs = require('fs').promises;
 
 const stylesFolder = path.resolve(__dirname, 'styles');
 const bundleStylees = path.resolve(__dirname, 'project-dist', 'style.css');
+const HTMLFile = path.resolve(__dirname, 'template.html');
+const componentsHTML = path.resolve(__dirname, 'components');
+const boundleHTML = path.resolve(__dirname, 'project-dist', 'index.html');
 
 (async function() {
   await fs.mkdir(path.resolve(__dirname, 'project-dist'), { recursive: true });
@@ -21,7 +24,25 @@ async function copyStyles(stylesFolder, bundle) {
   }
 
   await fs.writeFile(bundle, copy, 'utf8');
-  console.log(`Bundle is created`);
+}
+
+async function createHTML(componentsHTML, bundle, HTMLFile) {
+  let copy;
+  const html = await fs.readFile(HTMLFile, 'utf8');
+
+  const takeComponent = html.match(/{{(.*?)}}/g);
+
+  copy = html;
+
+  for (let component of takeComponent) {
+    const clearName = component.slice(2, -2) + '.html';
+    const allDataOfComp = await fs.readFile(path.resolve(componentsHTML, clearName), 'utf-8');
+    
+    copy = copy.replace(component, allDataOfComp);
+  }
+
+  await fs.writeFile(bundle, copy, 'utf8');
 }
 
 copyStyles(stylesFolder, bundleStylees);
+createHTML(componentsHTML, boundleHTML, HTMLFile);
