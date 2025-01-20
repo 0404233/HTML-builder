7762be51129820bcc6a5062786200d68d0f0c0ba
0404233 <andrey0404233@gmail.com>
2025-01-21 02:50:37 +0300

    feat: implement copyAssets fucnc in 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,8 +6,10 @@ const bundleStylees = path.resolve(__dirname, 'project-dist', 'style.css');
 const HTMLFile = path.resolve(__dirname, 'template.html');
 const componentsHTML = path.resolve(__dirname, 'components');
 const boundleHTML = path.resolve(__dirname, 'project-dist', 'index.html');
+const assetsFolder = path.resolve(__dirname, 'assets');
+const boundleAssets = path.join(__dirname, 'project-dist', 'assets');
 
-(async function() {
+(async function () {
   await fs.mkdir(path.resolve(__dirname, 'project-dist'), { recursive: true });
 })()
 
@@ -37,12 +39,37 @@ async function createHTML(componentsHTML, bundle, HTMLFile) {
   for (let component of takeComponent) {
     const clearName = component.slice(2, -2) + '.html';
     const allDataOfComp = await fs.readFile(path.resolve(componentsHTML, clearName), 'utf-8');
-    
+
     copy = copy.replace(component, allDataOfComp);
   }
 
   await fs.writeFile(bundle, copy, 'utf8');
 }
 
+async function copyAssets(assetsFolder, bundle) {
+  const queue = [{ main: assetsFolder, dist: bundle }];
+
+  while (queue.length > 0) {
+    const { main, dist } = queue.pop();
+
+    const files = await fs.readdir(main, { withFileTypes: true });
+
+    await fs.mkdir(dist, { recursive: true });
+
+    for (let file of files) {
+      const mainFile = path.join(main, file.name);
+      const bundleFile = path.join(dist, file.name);
+
+      if (file.isDirectory()) {
+        queue.push({ main: mainFile, dist: bundleFile });
+      } else {
+        await fs.copyFile(mainFile, bundleFile);
+      }
+    }
+  }
+}
+
+
 copyStyles(stylesFolder, bundleStylees);
 createHTML(componentsHTML, boundleHTML, HTMLFile);
+copyAssets(assetsFolder, boundleAssets);
